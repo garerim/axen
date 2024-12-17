51f661ce789f3d6757037bfd29bd66066108d25c
Bouvier <karl.bouvier4@laposte.net>
2024-12-17 14:09:36 +0100

    feat: add Tailwind CSS configuration and create reusable Button component

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
@@ -6,4 +7,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react(), TanStackRouterVite(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
