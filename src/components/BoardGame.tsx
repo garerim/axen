eeba0cdcf0b528b8c4defdaf0639f7ae8c4f5b6f
Bouvier <karl.bouvier4@laposte.net>
2024-12-20 13:44:42 +0100

    feat: add confetti effect for werewolf victory in BoardGame component

--- src/components/BoardGame.tsx
+++ src/components/BoardGame.tsx
@@ -73,6 +73,43 @@ export default function BoardGame() {
         };
      
         frame();
+    } else if (winner === "werewolf") {
+        const scalar = 2;
+        const blood = confetti.shapeFromText({ text: "🩸", scalar });
+     
+        const defaults = {
+          spread: 360,
+          ticks: 60,
+          gravity: 0,
+          decay: 0.96,
+          startVelocity: 20,
+          shapes: [blood],
+          scalar,
+        };
+     
+        const shoot = () => {
+          confetti({
+            ...defaults,
+            particleCount: 30,
+          });
+     
+          confetti({
+            ...defaults,
+            particleCount: 5,
+          });
+     
+          confetti({
+            ...defaults,
+            particleCount: 15,
+            scalar: scalar / 2,
+          });
+        };
+     
+        setTimeout(shoot, 0);
+        setTimeout(shoot, 100);
+        setTimeout(shoot, 200);
+        setTimeout(shoot, 300);
+        setTimeout(shoot, 500);
     }
   }, [winner])
 
